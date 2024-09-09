ff22fc0803595e76e2f1fadf61dcc6b383e3398a
Arvin <almarioarvhine.014@gmail.com>
2024-09-09 22:13:23 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+
+//loops
+//do while
+//less than = increment
+//greater than = devrement
+$num = 6;
+
+// do{
+//     echo 'Numero ' .$num .'<br>';
+//     $num--;
+// }while($num >= 1);
+
+while($num <= 5){
+    echo 'Numero ' .$num .'<br>';
+    $num++;
+}
+// Initialize,condition,iteration
+// for($num1=1;$num1<=5;$num1++){
+//     echo 'Numero ' .$num1 .'<br>';
+// }
+
+ 
+
+
+//create a function that returns loop
+
+
+// function myFunc($param1,$param2,$param3,$param4){
+
+//     $arr = [$param1, $param2, $param3, $param4];
+
+//     foreach($arr as $ar){
+//         echo 'Numero ' . $ar .'<br>';
+//     }
+// }
+
+// echo myFunc(34,33,56,56);
+
+
+
+?>
+
+
+
